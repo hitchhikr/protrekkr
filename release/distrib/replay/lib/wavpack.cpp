6287f8df0b1c41e685595c57acad522419417292
hitchhikr <hhhikr hhhikr@gmail.com>
2021-07-31 11:13:10 +0200

    bump copyright year

--- release/distrib/replay/lib/wavpack.cpp
+++ release/distrib/replay/lib/wavpack.cpp
@@ -2,7 +2,7 @@
 // Protrekkr
 // Based on Juan Antonio Arguelles Rius's NoiseTrekker.
 //
-// Copyright (C) 2008-2014 Franck Charlet.
+// Copyright (C) 2008-2021 Franck Charlet.
 // All rights reserved.
 //
 // This file is :
