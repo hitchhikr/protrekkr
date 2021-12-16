bff42d5a9496c40c760f120d417af89676f337b1
hitchhikr <hhhikr hhhikr@gmail.com>
2021-12-16 13:58:57 +0100

    maintenance

--- makedistrib_linux.sh
+++ makedistrib_linux.sh
@@ -1,5 +1,5 @@
 cd release/distrib/
-zip -vr ptk_v2.6.0_linux.zip * -x@../../exclude_linux.lst
+zip -vr ptk_v2.6.1_linux.zip * -x@../../exclude_linux.lst
 cd ..
 cd ..
 
--- makedistrib_macosx.sh
+++ makedistrib_macosx.sh
@@ -1,4 +1,4 @@
 cd release/distrib/
-zip -vr ptk_v2.6.0_macosx.zip * -x@../../exclude_macosx.lst
+zip -vr ptk_v2.6.1_macosx.zip * -x@../../exclude_macosx.lst
 cd ..
 cd ..
