e5986f30a2c527cade7d7943d8538688b8a1b5b6
unknown <charlet.franck@wanadoo.fr>
2024-05-10 20:19:54 +0200

    more tests

--- src/support/main.cpp
+++ src/support/main.cpp
@@ -974,11 +974,15 @@ extern SDL_NEED int SDL_main(int argc, char *argv[])
         glFlush();
         glFinish();
         glDrawBuffer(GL_FRONT);
+        glFlush();
+        glFinish();
         glRasterPos2f(-1.0f, -1.0f);
         glCopyPixels(0, 0, Cur_Width, Cur_Height, GL_COLOR);
         glFlush();
         glFinish();
         glDrawBuffer(GL_BACK);
+        glFlush();
+        glFinish();
 #else
         SDL_GL_SwapBuffers();
 #endif
